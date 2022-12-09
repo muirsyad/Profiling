98bce9baaaf70ba40e0c90ec16f00710f9c7db69
Muhammad Irsyad Bin Borhanuddin <muirsyad2399@gmail.com>
2022-12-09 17:12:00 +0800

    update_plot

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -407,6 +407,7 @@ class adminController extends Controller
     {
 
         $clients = DB::table('clients')->where('id', $clients->id)->first();
+        dd($clients);
         $participants = DB::table('users')->where('client_id', $clients->id)->get();
         $department = DB::table('departments')->get();
         $countre = DB::table('answer_records')->where('client_id', $clients->id)->count();
